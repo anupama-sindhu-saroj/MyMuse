460e41fc75c68f41034afb117a35e90e82860c5e
Techanushka <anushkabhasker12@gmail.com>
2026-03-10 14:46:57 +0530

    push8

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,20 +2,26 @@ import { useEffect } from "react";
 import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
+
 import { ThemeProvider } from "./context/ThemeContext";
+
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import InfoSection from "./components/InfoSection";
 import Features from "./components/Features";
 import Footer from "./components/Footer";
+
 import UserAuth from "./pages/user/UserAuth";
 import UserForgotPassword from "./pages/user/UserForgotPassword";
+
 import AdminGateway from "./pages/AdminGateway";
 import Dashboard from "./pages/Dashboard";
+
 import Explore from "./pages/Explore";
 import BookTicket from "./pages/BookTicket";
 import PaymentPage from "./pages/PaymentPage";
 import TicketPage from "./pages/TicketPage";
+
 import MuseumSignup from "./pages/MuseumSignup";
 import MuseumLogin from "./pages/MuseumLogin";
 import MuseumForgotPassword from "./pages/MuseumForgotPassword";
@@ -37,7 +43,7 @@ function App() {
     AOS.refresh();
   }, [location]);
 
-  // 🔐 Hidden Admin Shortcut (Ctrl + Shift + A)
+  // 🔐 Hidden Admin Shortcut
   useEffect(() => {
 
     const handleShortcut = (e) => {
@@ -55,8 +61,33 @@ function App() {
 
   }, [navigate]);
 
+  // 🚫 Remove Google Translate Top Banner
+  useEffect(() => {
+
+    const removeBanner = () => {
+
+      const banner = document.querySelector(".goog-te-banner-frame");
+
+      if (banner) {
+        banner.remove();
+      }
+
+      document.body.style.top = "0px";
+
+    };
+
+    const interval = setInterval(removeBanner, 500);
+
+    return () => clearInterval(interval);
+
+  }, []);
+
   return (
     <ThemeProvider>
+
+      {/* Hidden Google Translate Element */}
+      <div id="google_translate_element" style={{ display: "none" }}></div>
+
       <div className="min-h-screen bg-white text-black dark:bg-black dark:text-white transition-colors duration-500">
 
         {location.pathname !== "/explore" &&
@@ -78,16 +109,15 @@ function App() {
           <Route path="/userauth" element={<UserAuth />} />
           <Route path="/user-forgot-password" element={<UserForgotPassword />} />
 
-          {/* Dashboard */}
           <Route path="/dashboard" element={<Dashboard />} />
 
           {/* Hidden Admin */}
           <Route path="/admin" element={<AdminGateway />} />
 
-          {/* Explore Museums */}
+          {/* Explore */}
           <Route path="/explore" element={<Explore />} />
 
-          {/* Book Ticket */}
+          {/* Booking */}
           <Route path="/book" element={<BookTicket />} />
 
           {/* Payment */}
@@ -96,6 +126,7 @@ function App() {
           {/* Ticket */}
           <Route path="/ticket" element={<TicketPage />} />
 
+          {/* Museum Auth */}
           <Route path="/museum-signup" element={<MuseumSignup />} />
           <Route path="/museum-login" element={<MuseumLogin />} />
           <Route path="/museum-forgot-password" element={<MuseumForgotPassword />} />
@@ -107,6 +138,7 @@ function App() {
          location.pathname !== "/museum-forgot-password" && <Footer />}
 
       </div>
+
     </ThemeProvider>
   );
 }
