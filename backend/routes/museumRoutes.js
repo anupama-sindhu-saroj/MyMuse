320f0d83460aa7877a53013d6909acfa793e7f35
Techanushka <anushkabhasker12@gmail.com>
2026-03-20 00:09:09 +0530

    frontend only

--- backend/routes/museumRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { signupMuseum, loginMuseum } from "../controllers/museumController.js";
-
-const router = express.Router();
-
-router.post("/signup", signupMuseum);
-router.post("/login", loginMuseum);
-
-export default router;
