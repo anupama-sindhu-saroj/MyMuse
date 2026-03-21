97fa1f51a325d34ee2274fce300a58615620fdeb
Anushka Wanve <anushkawanve28@gmail.com>
2026-03-21 18:56:57 +0530

    My local changes

--- backend/models/Museum.js
+++ backend/models/Museum.js
@@ -2,10 +2,29 @@ import mongoose from "mongoose";
 
 const museumSchema = new mongoose.Schema({
   museumName: String,
-  email: { type: String, unique: true },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+
   phone: String,
   location: String,
-  password: String
+
+  password: String,
+
+  isVerified: {
+    type: Boolean,
+    default: false
+  },
+
+  otp: String,
+  otpExpiry: Date,
+
+  resetOTP: String,
+  resetOTPExpiry: Date
 });
 
 export default mongoose.model("Museum", museumSchema);
